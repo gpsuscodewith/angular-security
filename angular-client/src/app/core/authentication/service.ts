cf7b2292b3bc8c4bd1592b3f5dc76c6df38d6b5c
Kevin Hillinger <kevin.hillinger@microsoft.com>
2021-03-09 13:23:33 -0500

    init

---
+++ angular-client/src/app/core/authentication/service.ts
@@ -0,0 +1,92 @@
+import { Injectable } from '@angular/core';
+import { UserManager, User } from 'oidc-client';
+import { Constants } from '../../constants';
+import { Subject } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { AuthContext } from '../../model/auth-context';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationService {
+  private user: User;
+  private _loginChangedSubject = new Subject<boolean>();
+
+  loginChanged = this._loginChangedSubject.asObservable();
+  authContext: AuthContext;
+
+  constructor(private userManager: UserManager, private httpClient: HttpClient) {
+    this.userManager.events.addAccessTokenExpired(_ => {
+      this._loginChangedSubject.next(false);
+    });
+    this.userManager.events.addUserLoaded(user => {
+      if (this.user !== user) {
+        this.user = user;
+        this.loadSecurityContext();
+        this._loginChangedSubject.next(!!user && !user.expired);
+      }
+    });
+
+  }
+
+  login() {
+    return this.userManager.signinRedirect();
+  }
+
+  isLoggedIn(): Promise<boolean> {
+    return this.userManager.getUser().then(user => {
+      const userCurrent = !!user && !user.expired;
+      if (this.user !== user) {
+        this._loginChangedSubject.next(userCurrent);
+      }
+      if (userCurrent && !this.authContext) {
+        this.loadSecurityContext();
+      }
+      this.user = user;
+      return userCurrent;
+    });
+  }
+
+  completeLogin() {
+    return this.userManager.signinRedirectCallback().then(user => {
+      this.user = user;
+      this._loginChangedSubject.next(!!user && !user.expired);
+      return user;
+    });
+  }
+
+  logout() {
+    this.userManager.signoutRedirect();
+  }
+
+  completeLogout() {
+    this.user = null;
+    this._loginChangedSubject.next(false);
+    return this.userManager.signoutRedirectCallback();
+  }
+
+  getAccessToken() {
+    return this.userManager.getUser().then(user => {
+      if (!!user && !user.expired) {
+        return user.access_token;
+      }
+      else {
+        return null;
+      }
+    });
+  }
+
+  loadSecurityContext() {
+    this.httpClient
+      .get<AuthContext>(`${Constants.apiRoot}Projects/AuthContext`)
+      .subscribe(
+        context => {
+          this.authContext = new AuthContext();
+          this.authContext.claims = context.claims;
+          this.authContext.userProfile = context.userProfile;
+        },
+        error => console.error(error)
+      );
+  }
+
+}
---
+++ angular-client/src/app/home/signout-redirect-callback.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthenticationService } from '../core/authentication/service';
+
+@Component({
+  selector: 'app-signout-callback',
+  template: `<div></div>`
+})
+
+export class SignoutRedirectCallbackComponent implements OnInit {
+  constructor(private _authService: AuthenticationService,
+              private _router: Router) { }
+
+  ngOnInit() {
+    this._authService.completeLogout().then(_ => {
+      this._router.navigate(['/'], { replaceUrl: true });
+    })
+  }
+}
